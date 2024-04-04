37016ad27f65e3414cfda1d341662adf46f20b27
Tasmin Begum <tasminbegum@Jamils-MacBook-Pro.local>
2024-04-04 16:40:49 -0500

    first

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".banner-area").owlCarousel({
+        items:1,
+        loop:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:4000,
+        animateIn: 'fadeIn',
+        dots:false,
+      });
+  });
